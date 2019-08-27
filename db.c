ef5761785a516703ed569b3e9f77b65312be368e
stevenyangh <stevenyanmu@outlook.com>
2019-08-27 15:08:10 +0800

    db core update

--- db.c
+++ db.c
@@ -1,3 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define ROWLIMIT 100
+#define MANAGEDB 8
 typedef struct yymmddhhmmss
 {
     int year;
@@ -28,11 +32,39 @@ typedef struct table
     int floatNum;
     int tblNum;
     int rowNum;
+    int *defaultMap;
     tblrow *prow;
 } tbl;
 
-tbl* creat_table(void);
+tbl* init_resource(void);
+int free_resource(tbl* resource);
+
+tbl* creat_table(tbl *resource);
 void init_table(tbl *dirty_table);
-void set_table(tbl *clean_table, char *name, int keyIDColumn, int intNum, int strNum, int tblNum, int rowNum);
+void set_table(tbl *table, char *name, int keyIDColumn, int intNum, int strNum, int tblNum, int rowNum);
 int* calc_size(tbl tblInfo);
 void pack_row(int *size, int *pint, char **pstr, time *ptime, float *pflo, tbl *ptbl, int *tblcolumn);
+
+tbl* init_resource(void)
+{
+    tbl* newtbl;
+    newtbl = (tbl*)malloc(sizeof(tbl));
+    newtbl->prow = (tblrow*)malloc(ROWLIMIT * sizeof(tblrow));
+}
+int free_resource(tbl* resource)
+{
+
+}
+
+tbl* creat_table(tbl *resource)
+{
+    
+
+}
+
+void init_table(tbl *dirty_table);
+void set_table(tbl *table, char *name, int keyIDColumn, int intNum, int strNum, int tblNum, int rowNum);
+int* calc_size(tbl tblInfo);
+void pack_row(tbl *resource, int *size, int *pint, char **pstr, time *ptime, float *pflo, tbl *ptbl, int *tblcolumn);
+
+
