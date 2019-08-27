8a95b44d2adfb2751597b0b891f64ed72516a10d
stevenyangh <stevenyanmu@outlook.com>
2019-08-27 10:30:16 +0800

    fake db

--- db.c
+++ db.c
@@ -1,16 +1,21 @@
-typedef struct tabletitle
+typedef struct yymmddhhmmss
 {
-    char **intName;
-    char **strName;
-    char **tblName
-} tblttl;
+    int year;
+    int mon;
+    int day;
+    int h;
+    int min;
+    int sec;
+} time;
 
 typedef struct tablerow
 {
     int *pint;
     char **pstr;
+    time *ptime;
+    float *pflo;
     tbl *ptbl;
-    int keyID;
+    int *tblcolumn; //int this programe, key must be a number.
 } tblrow;
 
 typedef struct table
@@ -19,13 +24,15 @@ typedef struct table
     int keyIDColumn;
     int intNum;
     int strNum;
+    int timeNum;
+    int floatNum;
     int tblNum;
     int rowNum;
-    tblttl title;
     tblrow *prow;
 } tbl;
 
 tbl* creat_table(void);
 void init_table(tbl *dirty_table);
-void set_table(tbl *clean_table, int keyIDColumn, int intNum, int strNum, int tblNum, int rowNum, tblttl title, tblrow *row);
-void 
+void set_table(tbl *clean_table, char *name, int keyIDColumn, int intNum, int strNum, int tblNum, int rowNum);
+int* calc_size(tbl tblInfo);
+void pack_row(int *size, int *pint, char **pstr, time *ptime, float *pflo, tbl *ptbl, int *tblcolumn);
