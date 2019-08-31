909871a2855ed904d94ae698f00a85c7725f5be7
abyssdust <54522067+abyssdust@users.noreply.github.com>
2019-08-31 13:16:32 +0800

    Update dboperation.c

--- ShitMountain/dboperation.c
+++ ShitMountain/dboperation.c
@@ -1,18 +1,14 @@
 #ifndef DBOPERATION_C
 #define DBOPERATION_C
-
 #include"dbstruct.h"
-#include"arraysupport.c"
-#include<stdio.h>
+
+
 
 void displayInfo(tblinfo info);
-void setInfo(tblinfo *pinfo, const char* name, int intNum, int namNum, int timNum, int floNum, int tblNum, int rowNum);
+void setInfo(tblinfo *pinfo, const char* name, int intNum, int strNum, int timNum, int floNum, int tblNum, int rowNum);
 
 void displayItem(tblinfo info, nam *pitem);
-void setItem(tblinfo info, nam *ppitem);
-
-void displayChart();
-void addRow2Chart();   
+void setItem(tblinfo info, nam **ppitem);
 
 void displayInfo(tblinfo info)
 {
@@ -49,11 +45,63 @@ void setInfo(tblinfo *pinfo, const char* name,
 
 void displayItem(tblinfo info, nam *pitem)
 {
-    
+    for(int i = 0; i < (info.intNum +  info.namNum + info.timNum + info.floNum); i++)
+    {
+        for(int j = 0; j < STRLENLIMIT; j++)
+            if(pitem[i].c[j] == '\0')
+            {
+                printf(" ");
+            }
+            else
+                printf("%c", pitem[i].c[j]);
+        printf("|");
+    }
 }
 void setItem(tblinfo info, nam **ppitem)
 {
 
+}
+
+tblclmh newTblclmh(tblinfo info)
+{
+    tblclmh newclmh;
+    nam blankNam;
+    for(int i = 0; i < STRLENLIMIT )
+    {
+        blankNam.c[i] = '\0';
+    }
+    time blankTime;
+    blankTime.yea = 0;
+    blankTime.mon = 0;
+    blankTime.day = 0;
+    blankTime.hou = 0;
+    blankTime.min = 0;
+    blankTime.sec = 0;
+
+    newclmh.phint = constructD1(intp, info.intNum, 0);
+    newclmh.phnam = constructD1(namp, info.namNum, blankNam);
+    newclmh.phtim = constructD1(timp, info.timNum, blankTime);
+    newclmh.phflo = constructD1(flop, info.floNum, 0.0);
+}
+
+void cpyTblclmh(tblinfo info, tblclmh clmh1, tblclmh clmh2)
+{
+    for(int i = 0; i < info.intNum; i ++)
+    {
+        clmh2.phint[i] = clmh1.phint[i];
+    }
+    for(int i = 0; i < info.namNum; i ++)
+    {
+        clmh2.phnam[i] = clmh1.phnam[i];
+    }
+    for(int i = 0; i < info.timNum; i ++)
+    {
+        clmh2.phtim[i] = clmh1.phtim[i];
+    }
+    for(int i = 0; i < info.floNum; i ++)
+    {
+        clmh2.phflo[i] = clmh1.phflo[i];
+    }
 }
 
 #endif
