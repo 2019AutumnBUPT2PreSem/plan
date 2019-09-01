4b5e6b50b9f0ef47b2bdbd499cb54c910a2140ae
stevenyangh <stevenyanmu@outlook.com>
2019-09-01 11:53:36 +0800

    virtual table design

--- ShitMountain/vdboperation.c
+++ ShitMountain/vdboperation.c
@@ -2,10 +2,24 @@
 
 typedef struct virtualTable
 {
-    int n;
+    vch vtbSchm;
+    idm vtbMeta;
+} vtb;
+
+typedef struct vtbschemetic
+{
+    int itmNum;
     tbl *ptbl;
     int *ncolumn;
-} vtbl;
+} vch;
+
+typedef struct IDmeta
+{
+    int tblNum;
+    tbl* tblList;
+    int *i2t; // the jth column in vtb will  correspond to i2t[j]th tbl element in tblList
+    int *iti; // the jth column in vtb will  correspond to i2t[j]th tbl element in tblList's iti[j]th column
+} idm;
 
 int gettype(tblinfo info, int n)
 {
