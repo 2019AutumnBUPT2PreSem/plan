b8b0e44cdd9a39d4bd7b41771b1935e3801b58ea
scarecrew <49812579+scarecrew@users.noreply.github.com>
2019-09-04 14:08:03 +0800

    Update dbstruct.h

--- ShitMountain/dbstruct.h
+++ ShitMountain/dbstruct.h
@@ -1,6 +1,5 @@
 #ifndef DBSTRUCT_H
 #define DBSTRUCT_H
-
 #define STRLENLIMIT 16
 #define TBLINFONUM 8
 #define EXPPT 20// expand per tim
@@ -14,28 +13,42 @@ typedef struct yymmddhhmmss
     int min;
     int sec;
 } tim;
+// support the function to store the time 
+
+typedef struct char16
+{
+    char c[STRLENLIMIT];
+} nam;
+//support the function to store name
+
 typedef struct tableinfo
 {
-    char* name;
+    nam name;
     int intNum;
     int namNum;
     int timNum;
+    int floNum;
     int rowNum;
 } tblinfo;
+//support the function to store the number of data
+
 typedef struct tablecolumn
 {
     int **phint; // pointer to the head of a list of int, all the same.
-    char ***phnam;
+    nam **phnam;
     tim **phtim;
+    float **phflo;
 } tblclmh; // column head
+//support the function to store each column head
 
 typedef struct table
 {
     tblinfo info;
-    char **pitem;
+    nam *pitem;
     int lrn; // located row number
     tblclmh clm;
 } tbl;
+//support the function to store the record about the form
 
 tblclmh giveBlankClmh(void)
 {
@@ -43,17 +56,34 @@ tblclmh giveBlankClmh(void)
     newclmh.phint = NULL;
     newclmh.phnam = NULL;
     newclmh.phtim = NULL;
+    newclmh.phflo = NULL;
 }
+//give the form a new blank column head
+
+nam giveBlankNam(void)
+{
+    nam blankNam;
+    for(int i = 0; i < STRLENLIMIT; i++)
+    {
+        blankNam.c[i] = '\0';
+    }
+    return blankNam;
+}
+//give the form head new blank head
+
 tblinfo giveBlankInfo(void)
 {
     tblinfo info;
-    info.name = NULL;
+    info.name = giveBlankNam();
     info.intNum = 0;
     info.namNum = 0;
     info.timNum = 0;
+    info.floNum = 0;
     info.rowNum = 0;
     return info;
 }
+//give the form new blank info
+
 tbl giveBlankTbl(void)
 {
     tbl table;
@@ -62,6 +92,8 @@ tbl giveBlankTbl(void)
     table.lrn = 0;
     table.clm = giveBlankClmh(); 
 }
+//give a new blank form
+
 tim giveBlankTim(void)
 {
     tim blankTim;
@@ -73,5 +105,5 @@ tim giveBlankTim(void)
     blankTim.sec = 0;
     return blankTim;
 }
-
+// give a new time to the form
 #endif
