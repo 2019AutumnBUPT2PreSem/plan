152bc4cb50e20f6d5a93960490668728d8d57e81
pladoc1204 <46627016+pladoc1204@users.noreply.github.com>
2019-09-05 19:40:38 +0800

    FUCK THIS SHIT

--- Liang/main.c
+++ Liang/main.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <Windows.h>
+#include"dbstruct.h"
+#include"arraysupport.c"
+#include"dboperation.c"
 #include"dbio.c"
 #include"utilsupport.c"
 #include"decoration.c" 
+#include"service.c"
 
 
 int loginType();//liuge
-void InputIDPW(char *ID,char *PW,int type);//shubing
+void InputIDPW(char **ID,char **PW,int type);//shubing
+int InputIDPW_iter(char **ID, char **PW, int type);
 int Check4User(tbl user,char* ID,char* PW);
 int Check4Admin(tbl admin,char* ID,char* PW);
 int userMainBody(tbl provider,tbl user,tbl billinfo,tbl telerecord,tbl netrecord,tbl admin,tbl set,tbl moneyrecord);
@@ -17,14 +22,19 @@ int adminMainBody(tbl provider,tbl user,tbl billinfo,tbl telerecord,tbl netrecor
 int loginType()
 {
 	int type = 0;
-    printf("Enter your identity: 1.Admin;\n 2.User;\n");
+    printf("Enter your identity: \n 1.Admin;\n 2.User;\n");
     printf("Or press 0 to exit this programme. Your choice:");
     scanf("%d",&type);
     getchar();
     return type;
 }
 
-void InputIDPW(char *ID, char *PW, int type)
+void InputIDPW(char **ID, char **PW, int type)
+{
+	while(InputIDPW_iter(ID, PW, type));
+}
+
+int InputIDPW_iter(char **ID, char **PW, int type)
 {
 	 printf("\nPlease input your ID: ");
 	 int count1=0;
@@ -32,7 +42,7 @@ void InputIDPW(char *ID, char *PW, int type)
 	 int i=0;
 	 char id[10];
 	 gets(id);
- 
+	
 	 for(i=0;i<strlen(id);i++)
 	 {
 		  if(id[i]>='0'&&id[i]<='9')
@@ -43,15 +53,16 @@ void InputIDPW(char *ID, char *PW, int type)
 	 if(count1==0||count2==0||count1+count2<6||count1+count2>10)
 	 {
 		  printf("Error.");
-		  InputIDPW(ID, PW, type);
+		  return 1;
 	 }
  
 	 printf("\nPlease input your password: ");
 	 char password[16];
 	 gets(password);
-	 
-	 ID=id;
-	 PW=password;
+	
+	 *ID=id;
+	 *PW=password;
+	 return 0;
 }
 
 int Check4User(tbl user,char* ID,char* PW)
@@ -192,59 +203,65 @@ int main()
 	
 	
 	
-	/*we gonna get read all table there*/
+	/*we gonna read all table there*/
 	int quit_prog=0;
 	int quit_menu=0;
 	int IDu=-1;
 	int IDa=-1;
 	int type=0;
-	char ID[STRLENLIMIT]={"\0"};
-	char PW[STRLENLIMIT]={"\0"};
+	char IDarray[STRLENLIMIT]={"\0"};
+	char PWarray[STRLENLIMIT]={"\0"};
+	char *ID = IDarray;
+	char *PW = PWarray;
 	while(quit_prog==0)
 	{
 		quit_menu=0;
 		IDu=-1;
 		IDa=-1;
-		//Welcome();//WELCOME SCREEN
 		type=loginType();
 		if(type!=0)
 		{
-			InputIDPW(ID,PW,type);//check whether the ID and PW is legal
-		}
-		
-		if(type==1)
-		{
-			IDu=Check4User(user,ID,PW);
-			if(IDu!=-1)
-			{
-				while(quit_menu==0)
-				{
-					quit_menu=userMainBody(provider,user,billinfo,telerecord,netrecord,admin,set,moneyrecord);
-				}
-			}
-			
-		}
-		else if(type==2)
-		{
-			IDa=Check4Admin(admin,ID,PW);
-			if(IDa!=-1)
-			{
-				while(quit_menu==0)
-				{
-					quit_menu=userMainBody(provider,user,billinfo,telerecord,netrecord,admin,set,moneyrecord);
-				}
-			}
-		}
-		else if(type==0)
-		{
-			quit_prog=1;//quit the program
+			InputIDPW(&ID,&PW,type);//check whether the ID and PW is legal
 		}
 		else
 		{
-			printf("You have input a wrong num,please retry!\n");
+			if(type==1)
+			{
+				IDu=Check4User(user,IDarray,PWarray);
+				if(IDu!=-1)
+				{
+					while(quit_menu==0)
+					{
+						quit_menu=userMainBody(provider,user,billinfo,telerecord,netrecord,admin,set,moneyrecord);
+					}
+				}
+				
+			}
+			else if(type==2)
+			{
+				IDa=Check4Admin(admin,ID,PW);
+				if(IDa!=-1)
+				{
+					while(quit_menu==0)
+					{
+						quit_menu=userMainBody(provider,user,billinfo,telerecord,netrecord,admin,set,moneyrecord);
+					}
+				}
+			}
+			else if(type==0)
+			{
+				quit_prog=1;//quit the program
+			}
+			else
+			{
+				printf("You have input a wrong num,please retry!\n");
+				
+			}
 		}
+		
 	
 	}
+	/*write table*/
 	printf("Thank you for using!\n");
 	return 0;
 }
